43ab40eac3d5ce827f280dd1392f0b7478cc10f5
jjikku <98324520+jjikku@users.noreply.github.com>
2022-08-17 00:05:51 +0530

    added / route

--- BackEnd_new/app.js
+++ BackEnd_new/app.js
@@ -6,16 +6,16 @@ const loginRouter = require("./src/routes/loginroute");
 const googleLoginRouter = require("./src/routes/googleloginroute");
 const signupRouter = require("./src/routes/signuproute");
 const homeRouter = require("./src/routes/homeroute");
-const strategyRouter = require('./src/routes/strategyroute');
+const strategyRouter = require("./src/routes/strategyroute");
 const singleStrategyRouter = require("./src/routes/singlestrategyroute");
-const deployRouter = require('./src/routes/deployroute');
+const deployRouter = require("./src/routes/deployroute");
 
-const usersRouter = require("./src/routes/usersroute")
-const singleUserRouter = require("./src/routes/singleuserroute")
-const editUsersRouter = require("./src/routes/editusersroute")
-const UserPurchaseRoutes = require('./src/routes/userpurchase');
+const usersRouter = require("./src/routes/usersroute");
+const singleUserRouter = require("./src/routes/singleuserroute");
+const editUsersRouter = require("./src/routes/editusersroute");
+const UserPurchaseRoutes = require("./src/routes/userpurchase");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = new express();
 
@@ -28,21 +28,21 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.use("/login", loginRouter);
+app.use("/", loginRouter);
 app.use("/googlelogin", googleLoginRouter);
 
 app.use("/signup", signupRouter);
-app.use("/strategy",strategyRouter);
-app.use("/singlestrategy",singleStrategyRouter);
-app.use("/deploy",deployRouter);
-app.use('/userpurchase',UserPurchaseRoutes);
+app.use("/strategy", strategyRouter);
+app.use("/singlestrategy", singleStrategyRouter);
+app.use("/deploy", deployRouter);
+app.use("/userpurchase", UserPurchaseRoutes);
 
 app.use("/users", usersRouter);
 
 app.use("/singleuser", singleUserRouter);
-app.use("/edituser",editUsersRouter);
+app.use("/edituser", editUsersRouter);
 
-
-const PORT = (process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server Ready on ${PORT}`);
